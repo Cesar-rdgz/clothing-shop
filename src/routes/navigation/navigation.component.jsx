8d758ca20d702dcda9addeec19ba7b020d5b5253
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2025-03-22 12:10:33 -0600

    chore: navigation bar work

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
+    <Fragment>
+      <div className='navigation'>
+        <Link className="logo-container" to='/'>
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
