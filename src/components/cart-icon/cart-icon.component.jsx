566c3c587e9173dc0275e62f07f0a9562b549ae1
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2025-05-03 00:24:34 -0600

    contexts folder renaming

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-import { CartContext } from "../../context/cart.context";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
